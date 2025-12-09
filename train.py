9c5b422cecf27427a072a5941e0c5243dfb03586
Kairi Horiguchi <j4345wqu@sit.ac.jp>
2025-12-09 15:47:40 +0900

    学習時間(1エボック)の計測

---
+++ train.py
@@ -0,0 +1,95 @@
+import time
+
+import matplotlib.pyplot as plt
+
+import torch
+from torchvision import datasets
+import torchvision.transforms.v2 as transforms
+
+import models
+
+ds_transform = transforms.Compose([
+    transforms.ToImage(),
+    transforms.ToDtype(torch.float32, scale = True)
+])
+
+ds_train = datasets.FashionMNIST(
+    root = 'data',
+    train = True,
+    download = True,
+    transform = ds_transform
+)
+ds_test = datasets.FashionMNIST(
+    root = 'data',
+    train = False,
+    download = True,
+    transform = ds_transform
+)
+
+batch_size = 64
+dataloader_train = torch.utils.data.DataLoader(
+    ds_train,
+    batch_size=batch_size,
+    shuffle = True
+)
+dataloader_test = torch.utils.data.DataLoader(
+    ds_test,
+    batch_size = batch_size
+)
+
+
+model = models.MyModel()
+
+loss_fn = torch.nn.CrossEntropyLoss()
+
+learning_rate = 1e-3
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+n_epochs = 20
+
+train_loss_log = []
+val_loss_log = []
+train_acc_log = []
+val_acc_log = []
+
+
+for epoch in range(n_epochs):
+    print(f'epoch {epoch+1}/{n_epochs}')
+
+    time_start = time.time()
+    train_loss = models.train(model,dataloader_train, loss_fn, optimizer)
+    time_end = time.time()
+    print(f'    training loss: {train_loss}({time_end - time_start}s)')
+    train_loss_log.append(train_loss)
+
+    val_loss = models.test(model, dataloader_test, loss_fn)
+    print(f'    validation loss: {val_loss}')
+    val_loss_log.append(val_loss)
+
+    train_acc = models.test_accuracy(model, dataloader_train)
+    print(f'    training accuracy: {train_acc*100:.3f}%')
+    train_acc_log.append(train_acc)
+
+    val_acc = models.test_accuracy(model, dataloader_train)
+    print(f'    validation accuracy: {val_acc*100:.3f}%')
+    val_acc_log.append(val_acc)
+
+plt.subplot(1, 2, 1)
+plt.plot(range(1, n_epochs+1), train_loss_log, label='train loss')
+plt.plot(range(1, n_epochs+1), val_loss_log, label='val loss')
+plt.xlabel('epochs')
+plt.xticks(range(1, n_epochs+1))
+plt.ylabel('loss')
+plt.grid()
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(range(1, n_epochs+1), train_acc_log, label='train acc')
+plt.plot(range(1, n_epochs+1), val_acc_log, label='val acc')
+plt.xlabel('epochs')
+plt.xticks(range(1, n_epochs+1))
+plt.ylabel('accuracy')
+plt.grid()
+plt.legend()
+
+plt.show()
